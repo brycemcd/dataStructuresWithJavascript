7432fed19909d3c323f69aa90d29f87ef57845f5
Bryce McDonnell <bryce@bridgetownint.com>
2015-05-06 21:37:12 -0400

    Arrays, Q3

--- arrays.js
+++ arrays.js
@@ -43,3 +43,70 @@ for(i = (arr.length-1); i >= 0; i--) {
 }
 
 console.log('backward', backward);
+
+// Q3
+// Note: I don't like this pattern much. Refactor
+function Temps() {
+    this.dataStore = [
+        [],
+        [],
+        [],
+        []
+    ];
+    this.add = add;
+    this.average = average;
+    
+    this.weekAverage = weekAverage;
+    this.arrAverage = arrAverage;
+    this.monthAverage = monthAverage;
+    this.averageByWeek = averageByWeek;
+}
+
+function add(temps, week) {
+    this.dataStore[week] = temps;
+}
+
+function monthAverage() {
+    // the requirements are such that we can do the easiest thing that could
+    // possibly work. If the requirements change, this can be refactored
+    // into something that enumerates
+    return (this.weekAverage(0) + 
+            this.weekAverage(1) + 
+            this.weekAverage(2) + 
+            this.weekAverage(3)) / 4;
+}
+
+function weekAverage(weekNumber) {
+    return arrAverage( this.dataStore[weekNumber] );
+}
+
+function arrAverage(arr){
+  
+        var total = 0;
+        for(i = 0; i < arr.length; i++) {
+            total += arr[i];
+        }
+        return (total/arr.length);
+}
+
+function averageByWeek() {
+    return {
+        0: this.weekAverage(0),
+        1: this.weekAverage(1),
+        2: this.weekAverage(2),
+        3: this.weekAverage(3)
+    }
+}
+
+// add weeks worth of data at a time:
+var temps = new Temps();
+
+temps.add([21,22,23,24,25,26,27], 0);
+temps.add([31,32,33,34,35,36,37], 1);
+temps.add([41,42,43,44,45,46,47], 2);
+temps.add([51,52,53,54,55,56,57], 3);
+
+
+//temps.monthAverage();
+//temps.weekAverage(3);
+//temps.averageByWeek();
